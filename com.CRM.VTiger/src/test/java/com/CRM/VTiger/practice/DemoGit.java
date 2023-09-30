6d04e57fd853999ea9e0f61f2a9cd8f7be31d887
LisaPatel16 <lisapatel1617@gmail.com>
2023-09-30 16:02:21 +0530

    new demo git

---
+++ com.CRM.VTiger/src/test/java/com/CRM/VTiger/practice/DemoGit.java
@@ -0,0 +1,12 @@
+package com.CRM.VTiger.practice;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
